db480159c55f8580b439ad2da1b4628be919e414
Nibras <mohammed.nibras.2002@gmail.com>
2025-02-22 10:39:24 +0800

    renamed for faster testing

--- src/build.sh
+++ src/build.sh
@@ -6,11 +6,11 @@ mkdir -p ${BIN}
 
 rm ${BIN}processmanager
 rm ${BIN}clock
-rm ${BIN}dummyprog
+rm ${BIN}pr
 rm ${BIN}execpractice.c
 
 $GCC ${BIN}processmanager processmanager.c
 $GCC ${BIN}clock clock.c
-$GCC ${BIN}dummyprog dummyprog.c
+$GCC ${BIN}pr pr.c
 $GCC ${BIN}shell shell.c
 $GCC ${BIN}execpractice execpractice.c
